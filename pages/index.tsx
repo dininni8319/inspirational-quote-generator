f237f3ecb4c3eb3961f37f384317b6a3343d7618
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-07-14 15:09:39 +0200

    initial setup for NextJS project

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <>
+    {/* module that allows us to create dynamic header */}
+      <Head>  
+        <title>Inspirational Quote Generator</title>
+        <meta name="description" content="A fun project to generate quotes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
